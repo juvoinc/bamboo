dcca56d6dc17be6cd33c32bca100794874580a74
Aaron Mangum <aaron.mangum@juvo.com>
2019-08-15 15:39:39 -0700

    attribution

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name='bamboo',
     version=0.1,
+    author='Aaron Mangum',
+    author_email='aaron.mangum@juvo.com',
+    url='https://github.com/juvoinc/bamboo',
     packages=['bamboo'],
     python_requires='>=2.7',
     setup_requires=["pytest-runner", "flake8"],
