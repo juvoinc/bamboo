5dda154067f2c4f32ba95d3cd17144d9fc327a05
Aaron Mangum <mangum.aaron@gmail.com>
2020-01-18 11:02:27 -0800

    moved to apache license

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 """Install bamboo package."""
 from setuptools import setup
 
+VERSION = '0.3.1'
+
 setup(
     name='bamboo',
     packages=['bamboo'],
-    version='0.2.5',
-    license='MIT',
+    version=VERSION,
+    license='Apache License, Version 2.0',
     description='DataFrame interface for ElasticSearch',
     author='Aaron Mangum',
     author_email='aaron.mangum@juvo.com',
     url='https://github.com/juvoinc/bamboo',
-    download_url='https://github.com/juvoinc/bamboo/archive/0.2.5.tar.gz',
+    download_url='https://github.com/juvoinc/bamboo/archive/{}tar.gz'.format(VERSION),
     keywords=['elasticsearch', 'dataframe', 'pandas'],
     python_requires='>=2.7',
-    setup_requires=["pytest-runner", "flake8"],
-    tests_require=["pytest", "pytest-cov", "pandas"],
     install_requires=[
         'elasticsearch>=6.0.0'
     ],
@@ -22,8 +22,21 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-    ]
+    ],
+    extras_require=dict(
+        dev=[
+            'coverage',
+            'flake8',
+            'flake8-docstrings',
+            'pytest'
+        ],
+        test=[
+            'pandas',
+            'pytest',
+        ],
+        pandas=['pandas']
+    )
 )
